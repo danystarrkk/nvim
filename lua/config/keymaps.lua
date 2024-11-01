7933d2aa370605bca3fcf186bb2372760b37a9e5
stark <danystarrkk@gmail.com>
2024-11-01 12:39:44 -0500

    Cambios globales y de funcionalidad

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,14 @@ local keymap = vim.keymap.set
 --global keymaps
 
 keymapc("i", "jk", "<ESC>", { noremap = true }) --Esc
+
+-- Position cursor at the middle of the screen after scrolling half page
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+
+-- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
+keymap("i", "<C-b>", "<C-o>de")
+
 -- Live-server
 keymap("n", "<leader>ws", ":LiveServerStart<CR>")
 keymap("n", "<leader>wt", ":LiveServerStop<CR>")
@@ -17,3 +25,6 @@ keymap("n", "<leader>wl", ":LoremIpsum paragraphs")
 
 -- Auto comments visual line
 keymap("v", "<leader>wc", ":'<,'>CommentToggle<CR>")
+
+-----  OIL -----
+keymap("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
